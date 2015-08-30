aadec38534ddd3165bdd1b1a83838f24dd53e972
Jino <sonnysinzanity@gmail.com>
2015-08-30 14:21:29 +0800

    Added Runner.cpp

---
+++ Runner.cpp
@@ -0,0 +1,18 @@
+/*
+* Runner serves as the frame/window of the game.
+* Preprocessed statement of Game.cpp is inluded
+* Contains the main method.
+*/
+#include <iostream>
+#include "Game.cpp"
+using namespace std;
+
+/*
+*main
+*
+*Method for running the game
+*/
+int main()
+{
+	
+}
