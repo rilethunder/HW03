25ae8846e5eb59934367b338baf03febd18147f5
Jino <sonnysinzanity@gmail.com>
2015-08-30 14:21:06 +0800

    Added Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,26 @@
+/*
+* This class is where the functions of the program are coded
+*
+*
+*
+*/
+#include <iostream>
+#include <string>
+
+/*
+* This function draws the field, depending on the number of rows given in the input
+* It will be called time and time again , with the change of the position of the token, until either player wins
+* @ parameter &a refers to the address of the given int
+*/
+void fieldMake(int &a)
+{
+}
+
+/*
+* The is the main method, where the sequence of the game will be coded
+* This is also where all inputs will be processed
+*/
+int main()
+{
+}
+
